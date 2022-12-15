1bdd4f174f158f90a43dd9d0607493d5fc348aa7
V1tale <elativ.c@gmail.com>
2022-12-15 16:51:30 +0100

    implementato header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,46 @@
 <header>
-    HEADER
+    <nav>
+        <div class="top d-flex justify-content-end">
+            <h5>DC Power</h5>
+            <h5>Additional DC sites</h5>
+        </div>
+        <div class="container">
+            <ul class="d-flex justify-content-between align-items-center">
+                <li class="">
+                    <a href="{{ route('home') }}"><img src="{{ Vite::asset('resources/img/dc-logo.png') }}"
+                            alt=""></a>
+                </li>
+                <li class="">
+                    <a href="{{ route('characters') }}">Characters</a>
+                </li>
+                <li class="">
+                    <a href="{{ route('comics') }}">Comics</a>
+                </li>
+                <li class="">
+                    <a href="{{ route('movies') }}">Movies</a>
+                </li>
+                <li class="">
+                    <a href="{{ route('games') }}">Games</a>
+                </li>
+                <li class="">
+                    <a href="{{ route('tv') }}">Tv</a>
+                </li>
+                <li class="">
+                    <a href="{{ route('collectibles') }}">Collectibles</a>
+                </li>
+                <li class="">
+                    <a href="{{ route('videos') }}">Videos</a>
+                </li>
+                <li class="">
+                    <a href="{{ route('fans') }}">Fans</a>
+                </li>
+                <li class="">
+                    <a href="{{ route('news') }}">News</a>
+                </li>
+                <li class="">
+                    <a href="{{ route('shop') }}">Shop</a>
+                </li>
+            </ul>
+    </nav>
+    </div>
 </header>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,51 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $title = config('comics.title');
+    $description = config('comics.description');
+    $thumb = config('comics.thumb');
+    $price = config('comics.price');
+    $series = config('comics.series');
+    $sales = config('comics.sale_date');
+    $type = config('comics.type');
+    // $artists = config('comics.artists');
+    // $writers = config('comics.writers');
+
+    return view('home', compact('title', 'description', 'thumb', 'price', 'series', 'sales', 'type'));
 })->name("home");
+
+Route::get('/characters', function () {
+    return view('characters');
+})->name("characters");
+
+Route::get('/comics', function () {
+    return view('comics');
+})->name("comics");
+
+Route::get('/collectibles', function () {
+    return view('collectibles');
+})->name("collectibles");
+
+Route::get('/fans', function () {
+    return view('fans');
+})->name("fans");
+
+Route::get('/games', function () {
+    return view('games');
+})->name("games");
+
+Route::get('/movies', function () {
+    return view('movies');
+})->name("movies");
+Route::get('/shop', function () {
+    return view('shop');
+})->name('shop');
+Route::get('/tv', function () {
+    return view('tv');
+})->name("tv");
+Route::get('/videos', function () {
+    return view('videos');
+})->name("videos");
+Route::get('/news', function () {
+    return view('news');
+})->name("news");
